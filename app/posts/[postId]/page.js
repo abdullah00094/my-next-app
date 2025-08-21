27d8d0d124542609d246edc18966f8231a9678a5
abdullah <abdullah.morsi94@gmail.com>
2025-08-21 17:29:50 +0300

    edit

--- app/posts/[postId]/page.js
+++ app/posts/[postId]/page.js
@@ -2,6 +2,11 @@ import PostDetails from "@/app/components/postDetails";
 import { Suspense } from "react";
 export default async function PostDetailsPage({params}){
     const postId = params.postId;
+    const loadingJsx = (
+        <div>
+            <h1>Wait a second...</h1>
+        </div>
+    );
     return(
         <div>
             <h1>post Details </h1>
